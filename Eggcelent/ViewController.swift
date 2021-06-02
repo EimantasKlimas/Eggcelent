f46c9deceffe07318d845b241e47855ecec225b3
Eimantas <eim.klimas@gmail.com>
2021-06-03 00:17:40 +0300

    Init

--- Eggcelent/ViewController.swift
+++ Eggcelent/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var timer = Timer()
+    var totalTime: Float = 0
+    var secondsPassed: Float = 0
+    
+    enum HardnessValue: Float {
+        case Soft = 300
+        case Medium = 420
+        case Hard = 720
+        
+        init(_ title: String?) {
+            switch title {
+            case "Soft"? : self = .Soft
+            case "Medium"? : self = .Medium
+            case "Hard"? : self = .Hard
+            default: self = .Soft
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        totalTime = HardnessValue.init(sender.currentTitle).rawValue
+        timer.invalidate()
+        
+        titleLabel.text = sender.currentTitle!
+        Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            let percentageProgess = secondsPassed/totalTime
+            
+            secondsPassed += 1
+            progressBar.progress = percentageProgess
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
+        }
+    }
 }
 
